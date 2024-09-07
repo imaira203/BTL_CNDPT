4d545b3d9f8ecb7038f3473c1a22c0f1225972c8
Aira <trankimcuong301@gmail.com>
2024-09-07 22:23:25 +0700

    limit lastest to 10

--- client/src/components/VideoContent.js
+++ client/src/components/VideoContent.js
@@ -24,7 +24,12 @@ function VideoContent({ movie }) {
       <div className='new-nation'>{movie.nation}</div>
       <div className='movie-footer'>
         <div className='new-year'>Năm: {movie.release_year}</div>
-        <div className='new-like'>{movie.likes}</div>
+        <div className='new-like'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+            </svg>
+            {movie.likes}
+        </div>
       </div>
     </div>
   );
